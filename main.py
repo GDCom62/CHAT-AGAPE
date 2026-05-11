b8ee5cb21729e37f6a5fcd893bfcbabc5d242ccc
William de Aguiar Campos <138343439+GDCom62@users.noreply.github.com>
2026-05-11 15:26:55 -0300

    Change default port from 5000 to 8080

--- main.py
+++ main.py
@@ -113,5 +113,5 @@ def handle_message(data):
 # --- INICIALIZAÇÃO ---
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     socketio.run(app, host='0.0.0.0', port=port)
